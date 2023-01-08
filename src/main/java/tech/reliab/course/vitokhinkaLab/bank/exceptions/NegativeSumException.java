4533481a9c211e20b28ee8435ed41fe45a8e73e3
Mesady <87661386+Mesady@users.noreply.github.com>
2023-01-08 16:59:33 +0300

    Update NegativeSumException.java

--- src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/NegativeSumException.java
+++ src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/NegativeSumException.java
@@ -3,5 +3,5 @@ package tech.reliab.course.vitokhinkaLab.bank.exceptions;
 /**
  * Исключение для случая, когда была передана отрицательная сумма для денежной операции
  */
-public class NegativeSumException extends Error{
+public class NegativeSumException extends RuntimeException{
 }
