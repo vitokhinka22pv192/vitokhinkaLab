a92ab4266e713404330bbe09def79b4956e0a011
Mesady <87661386+Mesady@users.noreply.github.com>
2023-01-08 16:59:23 +0300

    Update LendingTermsException.java

--- src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/LendingTermsException.java
+++ src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/LendingTermsException.java
@@ -4,5 +4,5 @@ package tech.reliab.course.vitokhinkaLab.bank.exceptions;
 /**
  * Исключение для случая, когда были нарушены условия кредитования
  */
-public class LendingTermsException extends Error{
+public class LendingTermsException extends RuntimeException{
 }
