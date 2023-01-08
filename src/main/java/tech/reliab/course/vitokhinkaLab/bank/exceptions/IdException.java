589255954da4209389ecd08ee896ff9ece176d1b
Mesady <87661386+Mesady@users.noreply.github.com>
2023-01-08 16:59:13 +0300

    Update IdException.java

--- src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/IdException.java
+++ src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/IdException.java
@@ -4,5 +4,5 @@ package tech.reliab.course.vitokhinkaLab.bank.exceptions;
 /**
  * Исключение для случая, когда был передан id не существующего объекта
  */
-public class IdException extends Error{
+public class IdException extends RuntimeException{
 }
