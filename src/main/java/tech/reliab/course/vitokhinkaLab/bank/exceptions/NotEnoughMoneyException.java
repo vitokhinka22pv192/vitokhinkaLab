233501a1ea238ced3becdaf0d9f131e0e62e5146
Mesady <87661386+Mesady@users.noreply.github.com>
2023-01-08 16:59:45 +0300

    Update NotEnoughMoneyException.java

--- src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/NotEnoughMoneyException.java
+++ src/main/java/tech/reliab/course/vitokhinkaLab/bank/exceptions/NotEnoughMoneyException.java
@@ -3,5 +3,5 @@ package tech.reliab.course.vitokhinkaLab.bank.exceptions;
 /**
  * Исключение для случая, когда недостаточно средств для выполнения операции
  */
-public class NotEnoughMoneyException extends Error{
+public class NotEnoughMoneyException extends RuntimeException{
 }
